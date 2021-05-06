db7ede67c46308f393727706452254233b37da16
Kyle Harrington <mdc@kyleharrington.com>
2021-05-06 10:35:15 +0200

    Add a sphere to the basic scene

--- python_basic.py
+++ python_basic.py
@@ -1,31 +1,7 @@
-from scyjava import jimport, config
+from scyjava import jimport
+from sciview_pyground import setup_sciview_dependencies
 
-# Instructions for setting up python env and testing:
-# [assumes conda is installed, and in root of git repo]
-# 1) conda env create -f environment.yml
-# 2) conda activate sciview
-# 3) python scripts/python_basic.py
-
-#print(os.environ['JAVA_HOME'])
-
-# Setup maven repositories
-config.add_repositories({'jitpack': 'https://jitpack.io'})
-config.add_repositories(
-    {'scijava.public': 'https://maven.scijava.org/content/groups/public'})
-
-# Import/load dependencies
-sciview_deps = [
-    'sc.iview:sciview:f4dd286', 'graphics.scenery:scenery:937ba10',
-    'org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.20',
-    'org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9'
-]
-
-dependencies = [
-    'net.imagej:imagej:2.1.0', 'sc.fiji:bigdataviewer-core:10.1.1-SNAPSHOT'
-] + sciview_deps
-
-for dep in dependencies:
-    config.add_endpoints(dep)
+setup_sciview_dependencies()
 
 # Setup classes
 HashMap = jimport('java.util.HashMap')
@@ -34,3 +10,6 @@ ImageJ = jimport('net.imagej.ImageJ')
 
 # Launch sciview
 sv = SciView.create()
+
+# Add a sphere to the scene
+sv.addSphere()
